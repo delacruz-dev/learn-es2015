621e2622ea0c632480fb9e04132f9253c46caa5c
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2016-02-21 12:31:29 +0100

    promises examples

--- session-4/00_animation_es5.js
+++ session-4/00_animation_es5.js
@@ -1,7 +1,12 @@
+
 /** ES5 example **/
+function runAnimation(position){
+  console.log('Moving to position...', position);
+};
+
 runAnimation(0);
 setTimeout(function() {
-    runAnimation(1);    
+    runAnimation(1);
     setTimeout(function() {
         runAnimation(2);
     }, 1000);
