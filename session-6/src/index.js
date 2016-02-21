29f21c2b8e82ff7a0b14125cbcf9c896bfaa15e5
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2016-02-21 11:19:25 +0100

    renamed variable

--- session-6/src/index.js
+++ session-6/src/index.js
@@ -1,18 +1,10 @@
 import Card from './components/card';
 import Pokedex from './domain';
 
-// TODO: Make a domain with use case mappings
-// Create a factory for use case, repository and PokemonEntity
-// Create Pokemon Entity
-// Create use case
-// Create Repository
-// Accesss pokemon api using promises
-// Map results into Pokemon Entities
-// Map Pokemon Entity Array to Card components
-const p = new Card({
+const card = new Card({
   title: 'Hello, World!',
   description: 'This is supposed to be a Pokemon Card',
   imageUrl: 'path/to/image.url'
 });
 
-document.body.appendChild(p.render());
+document.body.appendChild(card.render());
