213c1bc12e3521cec4f50bcb4217a1d17c853b0c
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2016-02-21 10:25:11 +0100

    pokemans infrastracture

--- session-6/src/components/card.js
+++ session-6/src/components/card.js
@@ -1,11 +1,11 @@
 class Card {
-  constructor(PokemonEntity){
-    this._title = PokemonEntity.title;
-    this._description = PokemonEntity.description;
-    this._imageUrl = PokemonEntity.imageUrl;
+  constructor({title, description, imageUrl} = {}){
+    this._title = title;
+    this._description = description;
+    this._imageUrl = imageUrl;
   }
 
-  render(){
+  render() {
     const div = document.createElement('div');
     div.class = 'Card';
     div.textContent = this._text;
